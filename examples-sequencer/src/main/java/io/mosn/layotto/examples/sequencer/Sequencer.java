3a72c58155e050e34a35ce7178fcbbbfa90d7df4
seeflood <349895584@qq.com>
2022-06-29 15:13:02 +0800

    chore: split examples module into different modules (#21)

--- examples-sequencer/src/main/java/io/mosn/layotto/examples/sequencer/Sequencer.java
+++ examples-sequencer/src/main/java/io/mosn/layotto/examples/sequencer/Sequencer.java
@@ -1,10 +1,8 @@
 package io.mosn.layotto.examples.sequencer;
 
-import io.mosn.layotto.examples.file.File;
 import io.mosn.layotto.v1.RuntimeClientBuilder;
 import io.mosn.layotto.v1.config.RuntimeProperties;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.junit.Assert;
 import spec.sdk.runtime.v1.client.RuntimeClient;
 import spec.sdk.runtime.v1.domain.sequencer.GetNextIdRequest;
 import spec.sdk.runtime.v1.domain.sequencer.GetNextIdResponse;
@@ -12,27 +10,37 @@ import spec.sdk.runtime.v1.domain.sequencer.SequencerOptions;
 
 public class Sequencer {
 
-    private static final Logger logger    = LoggerFactory.getLogger(Sequencer.class.getName());
-
-    static String               storeName = "redis";
-    static String               key  = "examples";
-    static SequencerOptions     options = new SequencerOptions();
+    static String storeName = "sequencer_demo";
+    static String key       = "examples";
 
     public static void main(String[] args) {
-        options.setOption(SequencerOptions.AutoIncrement.WEAK);
-
+        // construct client
         RuntimeClient client = new RuntimeClientBuilder()
                 .withPort(RuntimeProperties.DEFAULT_PORT)
                 .build();
 
+        // prepare request
         GetNextIdRequest getNextIdRequest = new GetNextIdRequest();
         getNextIdRequest.setStoreName(storeName);
         getNextIdRequest.setKey(key);
+        // set SequencerOptions
+        SequencerOptions options = new SequencerOptions();
+        options.setOption(SequencerOptions.AutoIncrement.STRONG);
         getNextIdRequest.setOptions(options);
 
-        GetNextIdResponse nextId = client.getNextId(getNextIdRequest);
+        long prev = 0L;
+        for (int i = 0; i < 10; i++) {
+            // send request
+            System.out.println("Try to get next id.Key: " + key);
+            GetNextIdResponse nextIdResponse = client.getNextId(getNextIdRequest);
 
-        System.out.println("id :"+nextId.getNextId());
+            // assert
+            long nextId = nextIdResponse.getNextId();
+            System.out.println("Next id: " + nextId);
+            Assert.assertTrue(nextId > prev);
+
+            prev = nextId;
+        }
     }
 
 }
