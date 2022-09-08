f562652ed6fa3f405d8db2b7382783d60c15c8e8
seeflood <zhou.qunli@foxmail.com>
2022-09-08 16:24:49 +0800

    feat: add Makefile script
    Signed-off-by: seeflood <zhou.qunli@foxmail.com>

---
+++ tools/scripts/compile-proto.sh
@@ -0,0 +1,13 @@
+echo "Downloading Layotto project......"
+if test -e layotto ; then
+  cd layotto && git pull
+  cd ..
+fi
+git clone https://github.com/mosn/layotto.git
+
+echo "Copy proto files from Layotto......"
+cp layotto/spec/proto/extension/v1/*/*.proto spec/src/main/proto
+cp layotto/spec/proto/runtime/v1/*.proto spec/src/main/proto
+
+echo "Compile the proto files......"
+mvn compile
